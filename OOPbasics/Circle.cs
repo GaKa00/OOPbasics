176540a7844d40f053d6bb44636b685bd0c43057
Gabriel Kassarp <gkassarp@gmail.com>
2025-09-27 09:04:16 +0200

    Add project files.

---
+++ OOPbasics/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPbasics
+{
+    internal class Circle
+    {
+
+        private int _radius;
+
+        public Circle(int radius)
+        {
+            _radius = radius;
+        }
+
+        public void GetArea()
+        {
+            double pi = Math.PI;
+            double area = pi * _radius * _radius;
+            Console.WriteLine((int)area); 
+        }
+    }
+}
---
+++ OOPbasics/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OOPbasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Circle C1 = new Circle(5);
+
+            Circle C2 = new Circle(6);
+
+            C1.GetArea();
+            C2.GetArea();
+        }
+    }
+}
